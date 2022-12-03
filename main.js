0ae8b42d0d4cadf3b6fc246d2e6b04a14eea06c3
HuyNChu <chuhuyrpi@gmail.com>
2022-12-02 19:21:29 -0500

    wrote code to add semesters to user chosen year

--- main.js
+++ main.js
@@ -10,6 +10,33 @@ addSembtn.addEventListener("click", (e) => {
   root.classList.remove("hidden");
 
   // Creating Semester Div
-  semesterDiv = document.createElement("div");
-  semesterDiv.classList;
+  const semesterDiv = document.createElement("div");
+  semesterDiv.classList.add("semester");
+  semesterDiv.innerHTML = `<h2>${semName}</h2>
+  <form action="">
+    <input type="text" class="course-input" id="course-input">
+    <select name="credit" id="credit">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4" selected>4</option>
+    </select>
+    <input id="submit" type="submit" value="Add Course">
+  </form>
+  <button> Delete Semester </button>`;
+  console.log(semesterDiv);
+  const addCourseBtn = semesterDiv.querySelector("#submit");
+  console.log(addCourseBtn);
+  addCourseBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(semesterDiv);
+    const courseName = semesterDiv.querySelector("#course-input").value;
+    const credit = semesterDiv.querySelector("#credit").value;
+    if (courseName.length == 0) {
+      alert("Please enter a course name");
+    } else {
+      // create course items here
+    }
+  });
+  root.appendChild(semesterDiv);
 });
