6cb7e54be7d94a3a6778de9e1888e03286215d4a
HuyNChu <chuhuyrpi@gmail.com>
2022-12-02 23:44:00 -0500

    finished adding and delete semester, able to add course

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ addSembtn.addEventListener("click", (e) => {
   e.preventDefault();
   const semName = document.querySelector("#sem-name-input").value;
   const year = document.querySelector("#class-year-select").value;
-  console.log(semName, year);
+  if (semName.length === 0) {
+    alert("Please Enter a Semester name");
+    return;
+  }
 
   // Select the correct year and unhide
   root = document.querySelector(`#${year}`);
@@ -12,7 +15,11 @@ addSembtn.addEventListener("click", (e) => {
   // Creating Semester Div
   const semesterDiv = document.createElement("div");
   semesterDiv.classList.add("semester");
-  semesterDiv.innerHTML = `<h2>${semName}</h2>
+  semesterDiv.innerHTML = `
+  <div class="container">
+    <h2>${semName}</h2>
+    <i class="fa-solid fa-xmark" id="delete-sem"></i>
+  </div>
   <form action="">
     <input type="text" class="course-input" id="course-input">
     <select name="credit" id="credit">
@@ -22,21 +29,50 @@ addSembtn.addEventListener("click", (e) => {
       <option value="4" selected>4</option>
     </select>
     <input id="submit" type="submit" value="Add Course">
-  </form>
-  <button> Delete Semester </button>`;
-  console.log(semesterDiv);
+    </form>
+    <div class="course-list" id="course-list"></div>`;
+
+  // Add remove semester button/icon
+  const deleteSemester = semesterDiv.querySelector("#delete-sem");
+  deleteSemester.addEventListener("click", (e) => {
+    semesterDiv.remove();
+  });
+
+  // Add course to semester
   const addCourseBtn = semesterDiv.querySelector("#submit");
   console.log(addCourseBtn);
   addCourseBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log(semesterDiv);
     const courseName = semesterDiv.querySelector("#course-input").value;
     const credit = semesterDiv.querySelector("#credit").value;
+
+    // Adding course
     if (courseName.length == 0) {
       alert("Please enter a course name");
     } else {
-      // create course items here
+      const courseItem = document.createElement("div");
+      courseItem.className = "container";
+      courseItem.innerHTML = `
+      <div class="course-container">
+        <div>${courseName}</div><div>${credit}</div>
+      </div>
+      `;
+
+      const trashIcon = document.createElement("i");
+      trashIcon.className = "fa-solid fa-trash";
+      const checkIcon = document.createElement("i");
+      checkIcon.className = "fa-regular fa-square-check";
+
+      const childDiv = document.createElement("div");
+      childDiv.appendChild(checkIcon);
+      childDiv.appendChild(trashIcon);
+      courseItem.append(childDiv);
+
+      const courseList = semesterDiv.querySelector("#course-list");
+      courseList.appendChild(courseItem);
     }
   });
-  root.appendChild(semesterDiv);
+
+  const yearContent = document.querySelector(`#${year}` + "-content");
+  yearContent.appendChild(semesterDiv);
 });
