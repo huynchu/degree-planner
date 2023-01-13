69391c34af287dc0f50ae3d331f8be15a3a3ffc4
HuyNChu <chuhuyrpi@gmail.com>
2023-01-12 23:43:42 -0500

    added more year options, testing git

--- src/components/SemesterForm.js
+++ src/components/SemesterForm.js
@@ -13,6 +13,8 @@ function SemesterForm() {
 
   const yearOptions = [
     { label: "Select a year...", value: "" },
+    { label: "2030", value: "2030" },
+    { label: "2029", value: "2029" },
     { label: "2028", value: "2028" },
     { label: "2027", value: "2027" },
     { label: "2026", value: "2026" },
